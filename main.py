eea3b3e7cb452387a40e586361dd314c99df64a8
David Diaz <david.diaz190799@gmail.com>
2025-06-13 07:01:07 -0500

    test2

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-while True:
-    break
+from Subly import SublyBackend
+from dotenv import load_dotenv
+import os
+from logger_config import logger
+
+load_dotenv(dotenv_path=".env", override=True)
+api_url = os.getenv("API_URL")
+tenant = os.getenv("TENANT_ID")
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+
+
+
+backend = SublyBackend(tenant, api_url, username, password)
+users = backend.get_users()
+for user in users:
+    print(user)
+
